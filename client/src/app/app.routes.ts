d353eff5b4967ffb2946b32eb0249b5de3e5db31
Arsenii <redkoarsenii1612@gmail.com>
2026-04-20 16:07:14 +0300

    feat: AR-80 added pages and configured routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {WelcomePageComponent} from "./pages/welcome-page/welcome-page.component";
+import {LoginComponent} from "./pages/login/login.component";
+import {SignupComponent} from "./pages/signup/signup.component";
+import {ForgotPasswordComponent} from "./pages/forgot-password/forgot-password.component";
+import {OverviewComponent} from "./pages/overview/overview.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: WelcomePageComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent},
+  {path: 'forgot-password', component: ForgotPasswordComponent},
+  {path: 'overview', component: OverviewComponent},
+];
