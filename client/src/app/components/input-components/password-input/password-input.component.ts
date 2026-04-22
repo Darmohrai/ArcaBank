61b9a6fe7a2fcb54d90d6751e314b0a398bf7e58
Arsenii <redkoarsenii1612@gmail.com>
2026-04-22 11:56:47 +0300

    feat: AR-80 added inputs components + downloaded PrimeNG library

--- client/src/app/components/input-components/password-input/password-input.component.ts
+++ client/src/app/components/input-components/password-input/password-input.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import {PasswordModule} from "primeng/password";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-password-input',
   standalone: true,
-  imports: [],
+  imports: [
+    PasswordModule,
+    FormsModule
+  ],
   templateUrl: './password-input.component.html',
   styleUrl: './password-input.component.css'
 })
 export class PasswordInputComponent {
-
+  value!: string;
 }
--- client/src/app/pages/welcome-page/welcome-page.component.ts
+++ client/src/app/pages/welcome-page/welcome-page.component.ts
@@ -5,6 +5,9 @@ import {
   PrimaryBtnSmallComponent
 } from "../../components/button-components/primary-btn-small/primary-btn-small.component";
 import {CheckboxComponent} from "../../components/button-components/checkbox/checkbox.component";
+import {EmailInputComponent} from "../../components/input-components/email-input/email-input.component";
+import {PasswordInputComponent} from "../../components/input-components/password-input/password-input.component";
+import {PhoneInputComponent} from "../../components/input-components/phone-input/phone-input.component";
 
 @Component({
   selector: 'app-welcome-page',
@@ -13,7 +16,10 @@ import {CheckboxComponent} from "../../components/button-components/checkbox/che
     PrimaryBtnComponent,
     SecondaryBtnComponent,
     PrimaryBtnSmallComponent,
-    CheckboxComponent
+    CheckboxComponent,
+    EmailInputComponent,
+    PasswordInputComponent,
+    PhoneInputComponent
   ],
   templateUrl: './welcome-page.component.html',
   styleUrl: './welcome-page.component.css'
